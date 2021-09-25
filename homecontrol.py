9f074114086cc6e6ff27c80616893e79c45d4b2f
Lawson C <lawson.conallin@gmail.com>
2021-09-25 18:06:00 +1000

    Update homecontrol.py

--- homecontrol.py
+++ homecontrol.py
@@ -1,1 +1,12 @@
-#HomeControl
+#HomeControl#
+
+#####IMPORTS#####
+
+#####def#########
+def filereset():
+    setupdat=setupr.read lines()
+#####SETUP#######
+print("HOMECONTROL")
+setupr = open("setup.dat")
+setupw = open("setup.dat", "w")
+setupr.open()
